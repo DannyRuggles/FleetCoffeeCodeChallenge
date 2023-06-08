a3954368b39f82dc8e4e52ef2266272ce25fb7f5
Danny Ruggles <dannyruggles@hotmail.com>
2023-06-08 16:29:51 -0300

    Upload
    Uploading the coding challenge

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String input = "option";
+        String result = FirstNonRepeatedCharacter.firstNonRepeated(input);
+        System.out.println("Result: " + result);
+    }
+}
